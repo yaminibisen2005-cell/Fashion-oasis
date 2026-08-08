fccd37a8f65cf79bb318207f1970be428b9f4112
Yamini Bisen <yaminibisen2005@gmail.com>
2026-08-08 12:44:43 +0530

    Update frontend API integration

--- frontend/src/pages/AdminAuth/AdminLogin.jsx
+++ frontend/src/pages/AdminAuth/AdminLogin.jsx
@@ -22,7 +22,9 @@ const AdminLogin = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/v1/auth/admin/login", {
+     const API_URL = import.meta.env.VITE_API_URL;
+
+const response = await fetch(`${API_URL}/api/v1/auth/admin/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
